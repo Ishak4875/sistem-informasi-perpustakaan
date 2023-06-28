5cb677276003b2a0ce76fa18f199fd07861d5c7b
Ishak4875 <ishakjaelani07@gmail.com>
2023-06-28 11:35:36 +0800

    menambahkan database

--- database/db_absen.sql
+++ database/db_absen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2023 at 02:59 AM
+-- Generation Time: Jun 28, 2023 at 05:35 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -43,9 +43,10 @@ CREATE TABLE `tbl_buku` (
 INSERT INTO `tbl_buku` (`id_buku`, `kode_buku`, `nama_buku`, `tipe_buku`, `penulis_buku`, `foto_buku`) VALUES
 (1, 'B1', 'Ilmu 1', 'Buku', 'Ishak', 'B1.jpg'),
 (2, 'B2', 'Ilmu Sosial Revisi', 'Buku', 'Jaelani', '1687702482.jpg'),
-(3, 'B3', 'Buku Sosial 2', 'Buku', 'Muhammad Ishak', '1687697620.png'),
-(4, 'B4', 'Buku Sosial 3', 'Buku', 'Muhammad Ishak', '1687697727.png'),
-(6, 'B6', 'Perbedaan Manusia dan Hewan', 'Artikel', 'Dia', '1687697822.jpg');
+(3, 'B3', 'Buku Sosial 3', 'Buku', 'Muhammad Ishak', '1687697620.png'),
+(6, 'B6', 'Perbedaan Manusia dan Hewan', 'Artikel', 'Dia', '1687697822.jpg'),
+(8, 'B8', 'Siapa Itu Tuhan', 'Artikel', 'Kamu', '1687759447.jpg'),
+(9, 'B9', 'Jurnal KNN', 'Jurnal', 'Mereka', '1687849894.jpg');
 
 -- --------------------------------------------------------
 
@@ -90,7 +91,7 @@ ALTER TABLE `tbl_peminjaman`
 -- AUTO_INCREMENT for table `tbl_buku`
 --
 ALTER TABLE `tbl_buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_peminjaman`
