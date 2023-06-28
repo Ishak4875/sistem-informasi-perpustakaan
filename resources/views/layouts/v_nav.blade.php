c95165aa779df3dd7e4723d59aec2f9947f5e29b
Ishak4875 <ishakjaelani07@gmail.com>
2023-06-28 11:34:15 +0800

    second commit

---
+++ resources/views/layouts/v_nav.blade.php
@@ -0,0 +1,22 @@
+<header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="/" class="logo d-flex align-items-center">
+        <img src="/{{'template'}}/assets/img/logo.png" alt="">
+        <span>FlexStart</span>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="/">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a class="getstarted scrollto" href="/login">Login</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/buku/edit/{id_buku}',[BukuController::class,'displayPageEditBuku'])
 Route::post('/buku/update/{id_buku}',[BukuController::class,'updateBuku']);
 Route::get('/buku/delete/{id_buku}',[BukuController::class,'deleteBuku']);
 Route::get('/buku/detail/{id_buku}',[BukuController::class,'displayPageDetailBuku']);
+Route::get('login/', function () {
+    return view('v_login');
+});
